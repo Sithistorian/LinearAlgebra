bfaeda76a2052886f49eb109f38ca993d772c09c
Tristan <tristanshawnjohnson@gmail.com>
2021-07-27 13:56:37 -0500

    Using Express

---
+++ server/controllers/example.js
@@ -0,0 +1,10 @@
+const models = require('../models');
+
+
+
+module.exports = {
+
+  //all controller code lives here
+
+
+}
---
+++ server/models/example.js
@@ -0,0 +1,7 @@
+const db = require('../database');
+
+module.exports = {
+
+//all model code for example lives here
+
+}

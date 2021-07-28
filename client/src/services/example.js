71a2faea6b882089238d031ba688c6df9ad376b3
Tristan <tristanshawnjohnson@gmail.com>
2021-07-27 21:07:25 -0500

    Got the boilderplate going

---
+++ client/src/services/example.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  test() {
+    console.log('Hello from example services');
+  }
+
+  //all services code for example lives here
+  //I would put get and post requests here any request to the server
+
+}

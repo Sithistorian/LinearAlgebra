b6dd77865e462c6d33336a757e6ab301b4e315a1
Tristan <tristanshawnjohnson@gmail.com>
2021-07-28 15:01:06 -0500

    Added some tests for next few methods

--- client/services/matrix.js
+++ client/services/matrix.js
@@ -7,12 +7,10 @@ class Matrix {
   }
 
   createMatrix (m = 3, n = 3, filler = 0) {
-    //input number of rows, number of columns and filler
+
     let matrix = new Array(m).fill(new Array(n).fill(filler));
 
-    return matrix
-
-    //output array of arrays
+    return matrix;
   }
 
 }
--- client/services/matrix.test.js
+++ client/services/matrix.test.js
@@ -1,8 +1,77 @@
 const { Matrix } = require('./matrix');
 
 describe('Matrix class', () => {
+
+  let default = new Matrix();
+  let matrix1 = new Matrix(4, 4, 1);
+  let matrix2 = new Matrix(5, 6, 2);
+  let matrix3 = new Matrix(6, 7, 3);
+
   test('Default Matrix should be a 3x3 matrix filled with 0s', () => {
-    let matrix = new Matrix().matrix;
-    expect(matrix).toStrictEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+    expect(default.matrix).toStrictEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
   })
+
+  //getNumberOfRows tests
+
+  test('getNumberOfRows should be a method on Matrix', () => {
+    expect(typeof default.getNumberOfRows()).toBe('function');
+  })
+
+  test('getNumberOfRows should return correct results', () => {
+    expect(default.getNumberOfRows()).toBe(3);
+    expect(matrix1.getNumberOfRows()).toBe(4);
+    expect(matrix2.getNumberOfRows()).toBe(5);
+    expect(matrix3.getNumberOfRows()).toBe(6);
+  })
+
+  //getNumberOfColumns tests
+
+  test('getNumberOfColumns should be a method on Matrix', () => {
+    expect(typeof default.getNumberOfColumns()).toBe('function');
+  })
+
+  test('getNumberOfColumns should return correct results', () => {
+    expect(default.getNumberOfColumns()).toBe(3);
+    expect(matrix1.getNumberOfColumns()).toBe(4);
+    expect(matrix2.getNumberOfColumns()).toBe(6);
+    expect(matrix3.getNumberOfColumns()).toBe(7);
+  })
+
+  //changeValueAtIndex tests
+
+  test('changeValueAtIndex should be a method on Matrix', () => {
+    expect(typeof changeValueAtIndex).toBe('function');
+  })
+
+  test('changeValueAtIndex should modify the matrix correctly', () => {
+    default.changeValueAtIndex(1, 1, 4);
+    expect(default.matrix).toStrictEqual([[4, 0, 0], [0, 0, 0], [0, 0, 0]]);
+    default.changeValueAtIndex(1, 1, 0);
+    expect(default.matrix).toStrictEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+
+  })
+
+  //getValueAtIndex tests
+
+  test('getValueAtIndex should be a method on Matrix', () => {
+    expect(typeof default.getValueAtIndex).toBe('function');
+  })
+
+  test('getValueAtIndex should return null for i = 0 or j = 0', () => {
+    expect(getValueAtIndex(0, 1)).toBeNull();
+    expect(getValueAtIndex(1, 0)).toBeNull();
+    expect(getValueAtIndex(0, 0)).toBeNull();
+  })
+
+  test('getValueAtIndex should return correct results', () => {
+    expect(default.getValueAtIndex(1, 1)).toBe(0);
+    expect(matrix1.getValueAtIndex(1, 1)).toBe(1);
+    expect(matrix2.getValueAtIndex(1, 1)).toBe(2);
+    expect(matrix3.getValueAtIndex(1, 1)).toBe(3);
+    default.changeValueAtIndex(1, 3, 9);
+    expect(default.getValueAtIndex(1, 3)).toBe(9):
+  })
+
+
+
 })
